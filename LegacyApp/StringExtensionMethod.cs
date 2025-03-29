d157acededf252ab61c9c004bd0479b5080e5f7c
shehab249 <s29999@pjwstk.edu.pl>
2025-03-29 15:08:31 +0100

    Added class for email validation

---
+++ LegacyApp/StringExtensionMethod.cs
@@ -0,0 +1,9 @@
+﻿namespace LegacyApp;
+
+public static class StringExtensionMethod
+{
+    public static bool IsValidEmail(this string email)
+    {
+        return email.Contains("@") && email.Contains(".");
+    }
+}
