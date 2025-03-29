b9d01376149e11d2ff5356b27a6591ff2a814b7c
shehab249 <s29999@pjwstk.edu.pl>
2025-03-29 16:10:27 +0100

    Added unit testing

---
+++ Legacy_app_unit_tests_xUnit/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Legacy_app_unit_tests_xUnit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ Legacy_app_unittests_xUnit/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Legacy_app_unittests_xUnit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
