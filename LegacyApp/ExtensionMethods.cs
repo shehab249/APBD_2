5c962e3427fd763007dd46c15f58f9f39c5dc1dc
shehab249 <s29999@pjwstk.edu.pl>
2025-03-29 15:48:13 +0100

    Refactored Code

--- LegacyApp/ExtensionMethods.cs
+++ LegacyApp/ExtensionMethods.cs
@@ -1,4 +1,6 @@
-﻿namespace LegacyApp;
+﻿using System;
+
+namespace LegacyApp;
 
 public static class ExtensionMethods
 {
@@ -20,5 +22,23 @@ public static class ExtensionMethods
         }
 
         return true;
+    }
+    
+    public static int CalculateAge(this DateTime birthDate, DateTime referenceDate)
+    {
+        int age = referenceDate.Year - birthDate.Year;
+
+        if (referenceDate.Month < birthDate.Month || 
+            (referenceDate.Month == birthDate.Month && referenceDate.Day < birthDate.Day))
+        {
+            age--;
+        }
+
+        return age;
+    }
+
+    public static bool IsAdult(this DateTime birthDate, DateTime referenceDate, int minAge = 21)
+    {
+        return birthDate.CalculateAge(referenceDate) >= minAge;
     }
 }
--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -5,21 +5,19 @@ namespace LegacyApp
 {
     public class UserService
     {
+        private readonly ClientRepository clientRepository;
+        private readonly UserCreditService userCreditService;
+        public UserService()
+        {
+            clientRepository = new ClientRepository();
+            userCreditService = new UserCreditService();
+        }
         public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
         {
-            firstName.IsValidName(lastName);
-            email.IsValidEmail();
-
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
-            if (age < 21)
-            {
-                return false;
-            }
-
-            var clientRepository = new ClientRepository();
+            if (!firstName.IsValidName(lastName)) return false;
+            if (!email.IsValidEmail()) return false;
+            if (!dateOfBirth.IsAdult(DateTime.Now)) return false;
+            
             var client = clientRepository.GetById(clientId);
 
             var user = new User
@@ -31,36 +29,27 @@ namespace LegacyApp
                 LastName = lastName
             };
 
-            if (client.Type == "VeryImportantClient")
-            {
-                user.HasCreditLimit = false;
-            }
-            else if (client.Type == "ImportantClient")
-            {
-                using (var userCreditService = new UserCreditService())
-                {
-                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
-                    user.CreditLimit = creditLimit;
-                }
-            }
+            if (client.Type == "VeryImportantClient") user.HasCreditLimit = false;
+            
             else
             {
                 user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditService())
+                using (userCreditService)
                 {
                     int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                    if (client.Type == "ImportantClient")
+                    {
+                        creditLimit *= 2;
+                    }
                     user.CreditLimit = creditLimit;
                 }
             }
 
-            if (user.HasCreditLimit && user.CreditLimit < 500)
-            {
-                return false;
-            }
-
+            if (user.HasCreditLimit && user.CreditLimit < 500) return false;
+            
             UserDataAccess.AddUser(user);
             return true;
         }
+        
     }
 }
